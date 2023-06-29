4a44d971d2f34f09233f37bf2b8c1c268a59e4d6
bfl <bfl@spyro-soft.com>
2023-06-29 11:44:10 +0200

    first commit

---
+++ src/app/shared/data-storage.service.ts
@@ -0,0 +1,37 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { map, tap } from 'rxjs/operators';
+
+import { Recipe } from '../recipes/recipe.model';
+import { RecipeService } from '../recipes/recipe.service';
+
+@Injectable({ providedIn: 'root' })
+export class DataStorageService {
+  private readonly BASE_URL = 'http://localhost:8085/recipes';
+
+  constructor(private http: HttpClient, private recipeService: RecipeService) {}
+
+  storeRecipes() {
+    const recipes = this.recipeService.getRecipes();
+    this.http
+      .put(
+        this.BASE_URL,
+        recipes
+      )
+      .subscribe(response => {
+        console.log(response);
+      });
+  }
+
+  fetchRecipes() {
+    return this.http
+      .get<Recipe[]>(
+        this.BASE_URL
+      )
+      .pipe(
+        tap(recipes => {
+          this.recipeService.setRecipes(recipes);
+        })
+      )
+  }
+}
---
+++ src/app/shopping-list/shopping-edit/shopping-edit.component.ts
@@ -0,0 +1,68 @@
+import {
+  Component,
+  OnDestroy,
+  ViewChild,
+  AfterViewInit
+} from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { Subscription } from 'rxjs';
+
+import { Ingredient } from '../../shared/ingredient.model';
+import { ShoppingListService } from '../shopping-list.service';
+
+@Component({
+  selector: 'app-shopping-edit',
+  templateUrl: './shopping-edit.component.html',
+  styleUrls: ['./shopping-edit.component.css']
+})
+export class ShoppingEditComponent implements AfterViewInit, OnDestroy {
+  @ViewChild('f', { static: false }) slForm!: NgForm;
+  subscription: Subscription;
+  editMode = false;
+  editedItemIndex: number;
+  editedItem: Ingredient = new Ingredient('', 0);
+
+  constructor(private slService: ShoppingListService) {
+    this.subscription = new Subscription();
+    this.editedItemIndex = 0;
+  }
+
+  ngAfterViewInit() {
+    this.subscription = this.slService.startedEditing
+      .subscribe((index: number) => {
+        this.editedItemIndex = index;
+        this.editMode = true;
+        this.editedItem = this.slService.getIngredient(index);
+        this.slForm.setValue({
+          name: this.editedItem.name,
+          amount: this.editedItem.amount
+        });
+      });
+  }
+
+  onSubmit(form: NgForm) {
+    const value = form.value;
+    const newIngredient = new Ingredient(value.name, value.amount);
+    if (this.editMode) {
+      this.slService.updateIngredient(this.editedItemIndex, newIngredient);
+    } else {
+      this.slService.addIngredient(newIngredient);
+    }
+    this.editMode = false;
+    form.reset();
+  }
+
+  onClear() {
+    this.slForm.reset();
+    this.editMode = false;
+  }
+
+  onDelete() {
+    this.slService.deleteIngredient(this.editedItemIndex);
+    this.onClear();
+  }
+
+  ngOnDestroy() {
+    this.subscription.unsubscribe();
+  }
+}
