fcab4ab8e8cbb401ae871955d5210ad28619a554
bfl <bfl@spyro-soft.com>
2023-06-29 13:25:50 +0200

    fixes

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -11,16 +11,16 @@ export class RecipeService {
 
   // private recipes: Recipe[] = [
   //   new Recipe(
-  //     'Tasty Schnitzel',
-  //     'A super-tasty Schnitzel - just awesome!',
-  //     'https://upload.wikimedia.org/wikipedia/commons/7/72/Schnitzel.JPG',
+  //     'Tasty burger',
+  //     'Super burger!',
+  //     'https://upload.wikimedia.org/wikipedia/commons/9/9a/Big_Mac_hamburger.jpg',
   //     [new Ingredient('Meat', 1), new Ingredient('French Fries', 20)]
   //   ),
   //   new Recipe(
-  //     'Big Fat Burger',
-  //     'What else you need to say?',
-  //     'https://upload.wikimedia.org/wikipedia/commons/b/be/Burger_King_Angus_Bacon_%26_Cheese_Steak_Burger.jpg',
-  //     [new Ingredient('Buns', 2), new Ingredient('Meat', 1)]
+  //     'Spaghetti',
+  //     'Spaghetti bolognese',
+  //     'https://upload.wikimedia.org/wikipedia/commons/8/83/Spaghetti_Bolognese.jpg',
+  //     [new Ingredient('Pasta', 2), new Ingredient('Sauce', 1)]
   //   )
   // ];
   private recipes: Recipe[] = [];
